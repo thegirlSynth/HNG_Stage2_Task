227f6ea3beeb116ddcc42dcb38d8a0cef1f0026a
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2024-07-06 02:20:12 +0100

    Create user model

---
+++ authenticate/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+User Model
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlachemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+def User(Base):
+    """
+    User Model
+    """
+
+    __tablename__ = "users"
+
+    userId = Column(String, unique=True, primary_key=True)
+    firstName = Column(String, nullable=False)
+    lastName = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    phone = Column(String)
+
+
