0f17a118408606d1a7b31fa2e4a83bd440ad8297
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2024-07-06 23:46:11 +0100

    Create __init__ file

---
+++ authenticate/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+"""
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_migrate import Migrate
+from config import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from .models import User, Organisation
